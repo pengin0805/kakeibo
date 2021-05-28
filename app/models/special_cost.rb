c8b8cf7ff624282d3c42f59ca834c151addeb10c
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-28 16:55:36 +0900

    特別費のバリデーション追加

--- app/models/special_cost.rb
+++ app/models/special_cost.rb
@@ -2,4 +2,13 @@ class SpecialCost < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :special_category
 
+  with_options presence: true do
+    validates :special_name
+    validates :price
+    validates :date
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :special_category_id
+  end
+
 end
