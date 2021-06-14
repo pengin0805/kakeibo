eac1a73ee454aae171a54cc2c7d7b0e7575b8e57
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-06-14 18:51:50 +0900

    time_zone修正

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,10 @@ Bundler.require(*Rails.groups)
 module Kakeibo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-    
+    # config.load_defaults 6.0
+
     # config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
+    # config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
