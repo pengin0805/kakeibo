8424c900c2e5f4355948d026c2587b90abdf27f1
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-06-29 08:24:15 +0900

    空登録のエラー修正

--- app/controllers/fixeds_controller.rb
+++ app/controllers/fixeds_controller.rb
@@ -44,6 +44,18 @@ class FixedsController < ApplicationController
     @variable_costs = VariableCost.where('extract(year from start_time) = ? AND extract(month from start_time) = ?', Time.now.year, Time.now.month)
     @fixed_cost = FixedCost.new(fixed_cost_params)
     @variable_cost = VariableCost.new
+    today = Date.today
+    beginning_of_week = Date.new(today.year, today.month, today.beginning_of_month.day).beginning_of_week(:monday)
+    @week1_sum = VariableCost.where(start_time:beginning_of_week...beginning_of_week+7).sum(:price)
+    @week2_sum = VariableCost.where(start_time:beginning_of_week+7...beginning_of_week+14).sum(:price)
+    @week3_sum = VariableCost.where(start_time:beginning_of_week+14...beginning_of_week+21).sum(:price)
+    @week4_sum = VariableCost.where(start_time:beginning_of_week+21...beginning_of_week+28).sum(:price)
+    @week5_sum = VariableCost.where(start_time:beginning_of_week+28...beginning_of_week+35).sum(:price)
+    @week6_sum = VariableCost.where(start_time:beginning_of_week+35...beginning_of_week+42).sum(:price)
+    @month_sum_variable = VariableCost.where(start_time:today.beginning_of_month...today.next_month.beginning_of_month).sum(:price)
+    @month_sum_fixed = FixedCost.where(month:today.beginning_of_month...today.next_month.beginning_of_month).sum(:price)
+    @month_sum_special = SpecialCost.where(date:today.beginning_of_month...today.next_month.beginning_of_month).sum(:price)
+    @month_status = 50000 - @month_sum_variable
     if @fixed_cost.save
       redirect_to root_path
     else
